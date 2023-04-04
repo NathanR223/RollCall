362cb5579083c4ffa764516ba52132f5efc87409
Nathan Romero <nrom@yahoo.com>
2023-04-04 13:57:53 -0700

    wk10HandsOn

--- assets/data/jscript.js
+++ assets/data/jscript.js
@@ -1,3 +1,6 @@
+var blnOption;
+var err;
+
 function getMenu() {
     // set variables
     var url = window.location.pathname;
@@ -9,7 +12,7 @@ function getMenu() {
         // create new list item
         var li = document.createElement('li');
 
-        // get page 
+        // get page
         var pageName = aryMenu[x][0];
 
         // set active status of tab
@@ -29,5 +32,50 @@ function getMenu() {
 
         //append li to ul
         nav.appendChild(li);
+    }
+}
+
+function getSemester() {
+    // set variables
+    var semesterId = document.getElementById('semesterList');
+    var option = document.createElement('option');
+
+    // add a placeholder option
+    option.value = 0;
+    option.text = 'Select a Semester';
+    semesterId.add(option);
+
+// populate semester list from array
+    for (x = 0; x < arySemesters.length; x++) {
+        option = document.createElement('option');
+        option.value = arySemesters[x][0];
+        option.text = arySemesters[x][1];
+        semesterId.add(option);
+    }
+}
+
+function getClass(objSemester) {
+    // set variables
+    var semesterId = objSemester.options[objSemester.selectedIndex].value;
+    var aryClasses = eval(`ary${semesterId}`);
+    var classId = document.getElementById('classList');
+    var option = document.createElement('option');
+
+    // reset class list
+    classId.length = 0;
+
+    if(blnOption) {
+        // add a placeholder option
+        option.value = 0;
+        option.text = 'Select a Class';
+        classId.add(option);
+    }
+
+    //populate class list from array
+    for (x = 0; x < aryClasses.length; x++) {
+        var option = document.createElement('option');
+        option.value = aryClasses[x][0];
+        option.text = aryClasses[x][1];
+        classId.add(option);
     }
 }
